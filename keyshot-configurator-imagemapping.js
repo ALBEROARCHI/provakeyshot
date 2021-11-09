25ff4bcffabbc2d81ee902346caa3882cf9013fa
ALBEROARCHI <60703218+ALBEROARCHI@users.noreply.github.com>
2021-11-09 16:15:36 +0100

    aggiunta sul sito keyshot

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,47 @@
+imagemappingdata =
+[
+    {
+        "image": "untitled.32.1.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "6+19",
+            "9+34"
+        ],
+        "studio": 657449481
+    },
+    {
+        "image": "untitled.32.2.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "6+19",
+            "9+34"
+        ],
+        "studio": 657449481
+    },
+    {
+        "image": "untitled.32.3.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "6+19",
+            "9+34"
+        ],
+        "studio": 657449480
+    },
+    {
+        "image": "untitled.32.4.jpg",
+        "modelsets": [
+            1004
+        ],
+        "multimaterials": [
+            "6+19",
+            "9+34"
+        ],
+        "studio": 657449480
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,51 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "allestimento%20dii%20materiali%201%201.png",
+                    "multimaterials": [
+                        "6+19",
+                        "9+34"
+                    ],
+                    "name": "Allestimento dii materiali 1 1"
+                },
+                {
+                    "checked": true,
+                    "icon": "allestimento%20dii%20materiali%201%202.png",
+                    "multimaterials": [
+                        "6+19",
+                        "9+34"
+                    ],
+                    "name": "Allestimento dii materiali 1 2"
+                }
+            ],
+            "name": "Allestimento dii materiali 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "Allestimento dii materiali 1"
+            ],
+            "modelset": 1004,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "xr4354.png",
+            "id": 657449480,
+            "name": "xr4354"
+        },
+        {
+            "checked": true,
+            "icon": "swerty.png",
+            "id": 657449481,
+            "name": "swerty"
+        }
+    ]
+};
